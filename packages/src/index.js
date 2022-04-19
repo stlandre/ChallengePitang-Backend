b22ac9512ef1f53e2b7d86d899fa2bf7ae93e765
André Santos <4ndreluiz29@gmail.com>
2022-04-19 09:22:07 -0300

    Test library node-localstorage

--- packages/src/index.js
+++ packages/src/index.js
@@ -3,22 +3,33 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import { LocalStorage } from 'node-localstorage'
 
 dotenv.config()
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
 app.use(morgan('dev'))
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Server is running on PORT ${PORT}`)
+// })
+
+const localStorage = new LocalStorage('./scratch')
+
+localStorage.setItem('myFirstKey', 'myFirstValue')
+localStorage.setItem('mySecondKey', 'mySecondValue')
+// localStorage.removeItem('myFirstKey')
+// localStorage.removeItem('mySecondKey')
+localStorage.clear()
+console.log(localStorage.length)
+console.log(localStorage.getItem('mySecondKey'))
